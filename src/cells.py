b05651fc4f30943e361d4e0a69132808ac8c8e4e
jet <jetlewis@ucdavis.edu>
2014-12-28 12:04:34 -0800

    Removed random print statements

--- src/cells.py
+++ src/cells.py
@@ -10,7 +10,6 @@ class cells:
         for x in range(col+1):
             self.filled[x][row]=1
     def rowFilled(self):
-        print("*")
         for y in range (self.row-1,0,-1):
             for x in range(self.col):
                 if self.filled[x][y]!=1:
@@ -18,7 +17,6 @@ class cells:
                 if x == self.col-1:
                     self.clear(y)
     def clear(self,y):
-        print("**")
         for x in range(self.col+1):
             self.filled[x][y] = 0
             self.image[x][y] = self.default
