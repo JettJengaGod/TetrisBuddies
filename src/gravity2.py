c7978605f0cffaf029d374a4540df427a790a50c
Winston Liang <winstoyl@uci.edu>
2014-12-28 10:22:10 -0800

    gravity with both time increment and time shortening increment as parameters

--- src/gravity2.py
+++ src/gravity2.py
@@ -1,13 +1,14 @@
 import pygame
 
 class gravity():
-    def __int__(self,acc):
+    def __int__(self,acc,inc):
         self._time = pygame.time.get_ticks()
         self._dropTime = acc
+        self._increment = inc
         
     def fall(self,block):
         if pygame.time.get_ticks() - self._time > self._dropTime:
             block.y += 32
             self._time = pygame.time.get_ticks()
-            self._dropTime -= 50
+            self._dropTime -= self._increment
             
